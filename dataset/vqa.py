23d50e0819a927f6b65eb576379b0f1673d6e94a
yuanzhong <yuanzhongjr@gmail.com>
2024-12-17 22:57:06 +0800

    resolve a bug after changing the name

--- dataset/vqa.py
+++ dataset/vqa.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from PIL import Image
 
-from dataset import load_dataset, concatenate_datasets
+from datasets import load_dataset, concatenate_datasets
 from dataset.base import BaseDataset
 
 
