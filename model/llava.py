b45f727c7ea205c5a9ee898a16a709254ee5175d
yuanzhong <yuanzhongjr@gmail.com>
2024-12-19 15:23:21 +0800

    update llava-med

--- model/llava.py
+++ model/llava.py
@@ -39,7 +39,6 @@ class LLaVA(ChatMetaModel):
     ):
         # Load models from pretrained weights. For inference only.
         model_base = self.args.model_base
-
         model_name = get_model_name_from_path(model_path)
 
         kwargs = {"device_map": device_map, **kwargs}
