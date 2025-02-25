1cc095c8df8e473a658159007a0ecae6e3884773
Ruinan Jin <rjin02@tealab-a100.ece.ubc.ca>
2025-02-25 13:18:16 -0800

    update scripts

--- script/ruinan/tea2/clip_img_lora.sh
+++ script/ruinan/tea2/clip_img_lora.sh
@@ -13,32 +13,32 @@
  
 ################################################################################
 
-export TRANSFORMERS_CACHE=/projects/rjin-mh/rjin02/.cache/huggingface/hub
-export HF_HOME=/projects/rjin-mh/rjin02/.cache/huggingface/hub
+# export TRANSFORMERS_CACHE=/projects/rjin-mh/rjin02/.cache/huggingface/hub
+# export HF_HOME=/projects/rjin-mh/rjin02/.cache/huggingface/hub
 
-##### Number of total processes 
-echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
-echo "Nodelist:= " $SLURM_JOB_NODELIST
-echo "Number of nodes:= " $SLURM_JOB_NUM_NODES
-echo "Ntasks per node:= "  $SLURM_NTASKS_PER_NODE
-echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
+# ##### Number of total processes 
+# echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
+# echo "Nodelist:= " $SLURM_JOB_NODELIST
+# echo "Number of nodes:= " $SLURM_JOB_NUM_NODES
+# echo "Ntasks per node:= "  $SLURM_NTASKS_PER_NODE
+# echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
 
-echo "SLURM_PROCID="$SLURM_PROCID
+# echo "SLURM_PROCID="$SLURM_PROCID
 
-export PATH=/pkgs/anaconda3/bin:$PATH
-export CUDA_LAUNCH_BLOCKING=1
-source activate
-conda activate /h/rjin02/.conda/envs/torch2.3
+# export PATH=/pkgs/anaconda3/bin:$PATH
+# export CUDA_LAUNCH_BLOCKING=1
+# source activate
+# conda activate /h/rjin02/.conda/envs/torch2.3
 
 
-export HOME=/projects/rjin-mh/rjin02
+# export HOME=/projects/rjin-mh/rjin02
 
-cd /projects/rjin-mh/rjin02/MedVLMBench
+# cd /projects/rjin-mh/rjin02/MedVLMBench
 
-nvidia-smi
+# nvidia-smi
 
-echo "Run started at:- "
-date
+# echo "Run started at:- "
+# date
 
 
 # Training
@@ -79,7 +79,7 @@ date
 #     --model BioMedCLIP --model_path not_given \
 #     --cache_dir ./cache \
 #     --num_train_epochs 50 \
-#     --learning_rate 5e-5
+#     --learning_rate 5e-3
 
 
 # python run_train.py \
@@ -131,14 +131,14 @@ date
 #     --learning_rate 5e-3
 
 
-python run_train.py \
-    --task diagnosis --usage clip-img-lora --dataset BreastMNIST --split train \
-    --image_path ./data \
-    --output_dir ./log \
-    --model BioMedCLIP --model_path not_given \
-    --cache_dir ./cache \
-    --num_train_epochs 50 \
-    --learning_rate 5e-5
+# python run_train.py \
+#     --task diagnosis --usage clip-img-lora --dataset BreastMNIST --split train \
+#     --image_path ./data \
+#     --output_dir ./log \
+#     --model BioMedCLIP --model_path not_given \
+#     --cache_dir ./cache \
+#     --num_train_epochs 50 \
+#     --learning_rate 5e-3
 
 
 # python run_train.py \
@@ -250,53 +250,53 @@ python run_train.py \
 
 
 # Camelyon17
-# python run_train.py \
-#     --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
-#     --image_path ./data/camelyon17_v1.0/patches \
-#     --output_dir ./log \
-#     --model CLIP --model_path not_given \
-#     --cache_dir ./cache \
-#     --num_train_epochs 50 \
-#     --learning_rate 5e-3
+CUDA_VISIBLE_DEVICES=0 python run_train.py \
+    --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
+    --image_path ./data/camelyon17_v1.0/patches \
+    --output_dir ./log \
+    --model CLIP --model_path not_given \
+    --cache_dir ./cache \
+    --num_train_epochs 20 \
+    --learning_rate 5e-3
 
-# python run_train.py \
-#     --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
-#     --image_path ./data/camelyon17_v1.0/patches \
-#     --output_dir ./log \
-#     --model BLIP --model_path not_given \
-#     --cache_dir ./cache \
-#     --num_train_epochs 50 \
-#     --learning_rate 5e-3
-
-
-# python run_train.py \
-#     --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
-#     --image_path ./data/camelyon17_v1.0/patches \
-#     --output_dir ./log \
-#     --model BLIP2-2.7b --model_path not_given \
-#     --cache_dir ./cache \
-#     --num_train_epochs 50 \
-#     --learning_rate 5e-3
+CUDA_VISIBLE_DEVICES=1 python run_train.py \
+    --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
+    --image_path ./data/camelyon17_v1.0/patches \
+    --output_dir ./log \
+    --model BLIP --model_path not_given \
+    --cache_dir ./cache \
+    --num_train_epochs 20 \
+    --learning_rate 5e-3 &
 
 
-# python run_train.py \
-#     --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
-#     --image_path ./data/camelyon17_v1.0/patches \
-#     --output_dir ./log \
-#     --model BioMedCLIP --model_path not_given \
-#     --cache_dir ./cache \
-#     --num_train_epochs 50 \
-#     --learning_rate 5e-3
+CUDA_VISIBLE_DEVICES=2 python run_train.py \
+    --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
+    --image_path ./data/camelyon17_v1.0/patches \
+    --output_dir ./log \
+    --model BLIP2-2.7b --model_path not_given \
+    --cache_dir ./cache \
+    --num_train_epochs 20 \
+    --learning_rate 5e-3 &
 
 
-# python run_train.py \
-#     --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
-#     --image_path ./data/camelyon17_v1.0/patches \
-#     --output_dir ./log \
-#     --model MedCLIP --model_path not_given \
-#     --cache_dir ./cache \
-#     --num_train_epochs 50 \
-#     --learning_rate 5e-3
+CUDA_VISIBLE_DEVICES=3 python run_train.py \
+    --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
+    --image_path ./data/camelyon17_v1.0/patches \
+    --output_dir ./log \
+    --model BioMedCLIP --model_path not_given \
+    --cache_dir ./cache \
+    --num_train_epochs 20 \
+    --learning_rate 5e-3 &
+
+
+CUDA_VISIBLE_DEVICES=4 python run_train.py \
+    --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
+    --image_path ./data/camelyon17_v1.0/patches \
+    --output_dir ./log \
+    --model MedCLIP --model_path not_given \
+    --cache_dir ./cache \
+    --num_train_epochs 20 \
+    --learning_rate 5e-3 &
 
 
 
--- script/ruinan/tea2/lp_train.sh
+++ script/ruinan/tea2/lp_train.sh
@@ -13,32 +13,32 @@
  
 ################################################################################
 
-export TRANSFORMERS_CACHE=/projects/rjin-mh/rjin02/.cache/huggingface/hub
-export HF_HOME=/projects/rjin-mh/rjin02/.cache/huggingface/hub
+# export TRANSFORMERS_CACHE=/projects/rjin-mh/rjin02/.cache/huggingface/hub
+# export HF_HOME=/projects/rjin-mh/rjin02/.cache/huggingface/hub
 
-##### Number of total processes 
-echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
-echo "Nodelist:= " $SLURM_JOB_NODELIST
-echo "Number of nodes:= " $SLURM_JOB_NUM_NODES
-echo "Ntasks per node:= "  $SLURM_NTASKS_PER_NODE
-echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
+# ##### Number of total processes 
+# echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
+# echo "Nodelist:= " $SLURM_JOB_NODELIST
+# echo "Number of nodes:= " $SLURM_JOB_NUM_NODES
+# echo "Ntasks per node:= "  $SLURM_NTASKS_PER_NODE
+# echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
 
-echo "SLURM_PROCID="$SLURM_PROCID
+# echo "SLURM_PROCID="$SLURM_PROCID
 
-export PATH=/pkgs/anaconda3/bin:$PATH
-export CUDA_LAUNCH_BLOCKING=1
-source activate
-conda activate /h/rjin02/.conda/envs/torch2.3
+# export PATH=/pkgs/anaconda3/bin:$PATH
+# export CUDA_LAUNCH_BLOCKING=1
+# source activate
+# conda activate /h/rjin02/.conda/envs/torch2.3
 
 
-export HOME=/projects/rjin-mh/rjin02
+# export HOME=/projects/rjin-mh/rjin02
 
-cd /projects/rjin-mh/rjin02/MedVLMBench
+# cd /projects/rjin-mh/rjin02/MedVLMBench
 
-nvidia-smi
+# nvidia-smi
 
-echo "Run started at:- "
-date
+# echo "Run started at:- "
+# date
 
 # Train Xray
 # python run_train.py \
@@ -253,63 +253,117 @@ date
 #     --learning_rate 5e-5
 
 # Camelyon17
-# python run_train.py \
+# CUDA_VISIBLE_DEVICES=7 python run_train.py \
 #     --task diagnosis --usage lp --dataset Camelyon17 --split train \
 #     --image_path ./data/camelyon17_v1.0/patches \
 #     --output_dir ./log \
 #     --model CLIP --model_path not_given \
 #     --cache_dir ./cache \
 #     --num_train_epochs 50 \
-#     --learning_rate 5e-5
+#     --learning_rate 5e-5 &
 
-# python run_train.py \
+# CUDA_VISIBLE_DEVICES=6 python run_train.py \
 #     --task diagnosis --usage lp --dataset Camelyon17 --split train \
 #     --image_path ./data/camelyon17_v1.0/patches \
 #     --output_dir ./log \
 #     --model BLIP --model_path not_given \
 #     --cache_dir ./cache \
 #     --num_train_epochs 50 \
-#     --learning_rate 5e-5
+#     --learning_rate 5e-5 &
 
-# python run_train.py \
+# CUDA_VISIBLE_DEVICES=5 python run_train.py \
 #     --task diagnosis --usage lp --dataset Camelyon17 --split train \
 #     --image_path ./data/camelyon17_v1.0/patches \
 #     --output_dir ./log \
 #     --model BLIP2-2.7b --model_path not_given \
 #     --cache_dir ./cache \
 #     --num_train_epochs 50 \
-#     --learning_rate 5e-5
+#     --learning_rate 5e-5 &
 
-# python run_train.py \
+# CUDA_VISIBLE_DEVICES=4 python run_train.py \
 #     --task diagnosis --usage lp --dataset Camelyon17 --split train \
 #     --image_path ./data/camelyon17_v1.0/patches \
 #     --output_dir ./log \
 #     --model BioMedCLIP --model_path not_given \
 #     --cache_dir ./cache \
 #     --num_train_epochs 50 \
-#     --learning_rate 5e-5
+#     --learning_rate 5e-5 &
 
-# python run_train.py \
+# CUDA_VISIBLE_DEVICES=3 python run_train.py \
 #     --task diagnosis --usage lp --dataset Camelyon17 --split train \
 #     --image_path ./data/camelyon17_v1.0/patches \
 #     --output_dir ./log \
 #     --model MedCLIP --model_path not_given \
 #     --cache_dir ./cache \
 #     --num_train_epochs 50 \
-#     --learning_rate 5e-5
+#     --learning_rate 5e-5 &
 
 
-python run_train.py \
-    --task diagnosis --usage lp --dataset Camelyon17 --split train \
-    --image_path ./data/camelyon17_v1.0/patches \
+# CUDA_VISIBLE_DEVICES=2 python run_train.py \
+#     --task diagnosis --usage lp --dataset Camelyon17 --split train \
+#     --image_path ./data/camelyon17_v1.0/patches \
+#     --output_dir ./log \
+#     --model PMCCLIP --model_path not_given \
+#     --cache_dir ./cache \
+#     --num_train_epochs 50 \
+#     --learning_rate 5e-5 &
+
+
+# ChestXray
+CUDA_VISIBLE_DEVICES=7 python run_train.py \
+    --task diagnosis --usage lp --dataset ChestXray --split train \
+    --image_path /mnt/sdc/rjin02/DataSets/chest_xray \
+    --output_dir ./log \
+    --model CLIP --model_path not_given \
+    --cache_dir ./cache \
+    --num_train_epochs 50 \
+    --learning_rate 5e-5 &
+
+CUDA_VISIBLE_DEVICES=6 python run_train.py \
+    --task diagnosis --usage lp --dataset ChestXray --split train \
+    --image_path /mnt/sdc/rjin02/DataSets/chest_xray \
+    --output_dir ./log \
+    --model BLIP --model_path not_given \
+    --cache_dir ./cache \
+    --num_train_epochs 50 \
+    --learning_rate 5e-5 &
+
+CUDA_VISIBLE_DEVICES=5 python run_train.py \
+    --task diagnosis --usage lp --dataset ChestXray --split train \
+    --image_path /mnt/sdc/rjin02/DataSets/chest_xray \
+    --output_dir ./log \
+    --model BLIP2-2.7b --model_path not_given \
+    --cache_dir ./cache \
+    --num_train_epochs 50 \
+    --learning_rate 5e-5 &
+
+CUDA_VISIBLE_DEVICES=4 python run_train.py \
+    --task diagnosis --usage lp --dataset ChestXray --split train \
+    --image_path /mnt/sdc/rjin02/DataSets/chest_xray \
+    --output_dir ./log \
+    --model BioMedCLIP --model_path not_given \
+    --cache_dir ./cache \
+    --num_train_epochs 50 \
+    --learning_rate 5e-5 &
+
+CUDA_VISIBLE_DEVICES=3 python run_train.py \
+    --task diagnosis --usage lp --dataset ChestXray --split train \
+    --image_path /mnt/sdc/rjin02/DataSets/chest_xray \
+    --output_dir ./log \
+    --model MedCLIP --model_path not_given \
+    --cache_dir ./cache \
+    --num_train_epochs 50 \
+    --learning_rate 5e-5 &
+
+
+CUDA_VISIBLE_DEVICES=2 python run_train.py \
+    --task diagnosis --usage lp --dataset ChestXray --split train \
+    --image_path /mnt/sdc/rjin02/DataSets/chest_xray \
     --output_dir ./log \
     --model PMCCLIP --model_path not_given \
     --cache_dir ./cache \
     --num_train_epochs 50 \
-    --learning_rate 5e-5
-
-
-
+    --learning_rate 5e-5 &
 
 
 
