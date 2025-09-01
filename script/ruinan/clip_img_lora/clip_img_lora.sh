dfd915414074a5a05cde0d48d1e5ed01b8111e36
Nanboy-Ronan <jinruinan@163.com>
2025-09-01 15:02:19 -0700

    update

--- script/ruinan/clip_img_lora/clip_img_lora.sh
+++
@@ -1,9 +0,0 @@
-# HAM10000
-CUDA_VISIBLE_DEVICES=4 python run_train.py \
-    --task diagnosis --usage clip-img-lora --dataset Camelyon17 --split train \
-    --image_path ./data/camelyon17_v1.0/patches \
-    --output_dir ./log \
-    --model CLIP --model_path not_given \
-    --cache_dir ./cache \
-    --num_train_epochs 10 \
-    --learning_rate 5e-5
